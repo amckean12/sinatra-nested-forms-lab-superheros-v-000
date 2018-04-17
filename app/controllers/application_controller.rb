50fb72c9a282f34cb825e56315b58b251cd40911
Alex McKean <amckean@me.com>
2018-04-17 14:05:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
 
+    get '/' do
+      erb :team
+    end
+
+    post '/teams' do
+      
+    end
+
+
+
 end
---
+++ app/models/team.rb
@@ -0,0 +1,14 @@
+class Team 
+
+  attr_accessor :name, :motto
+
+  @@teams = []
+
+  def initialize(args)
+    @name = args[:name]
+    @motto = args[:motto]
+    @@teams << self
+  end
+
+  
+end 
